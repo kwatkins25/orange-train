2c2932b1af729cf3a2e89aacf5ac4066e15ab232
Kania Watkins <144135880+kwatkins25@users.noreply.github.com>
2026-05-01 19:58:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,16 @@ let ok3button = document.querySelector(".ok3button");
 console.log(buttonstart);
 let playagainbutton = document.querySelector(".playagainbutton");
 console.log(buttonstart);
-let continuebutton= document.querySelector(".continuebutton");
+let continuebutton1= document.querySelector(".continuebutton1");
+let ok4button= document.querySelector(".ok4button");
+let continuebutton2= document.querySelector(".continuebutton2");
+let continuebutton3= document.querySelector(".continuebutton3");
+let continuebutton4= document.querySelector(".continuebutton4");
+let continuebutton5= document.querySelector(".continuebutton5");
+let continuebutton6= document.querySelector(".continuebutton6");
+let sourcesbutton= document.querySelector(".sourcesbutton")
+
+
 
 
 let titleScreen = document.querySelector(".titleScreen");
@@ -31,7 +40,17 @@ let askscreen = document.querySelector(".askscreen");
 let askmomscreen = document.querySelector(".askmomscreen");
 let thinkscreen = document.querySelector(".thinkscreen");
 let madeupscreen = document.querySelector(".madeupscreen");
-let playagainscreen = document.querySelector(".playagain");
+let playagainscreen = document.querySelector(".playagainscreen");
+let chooseAIscreen = document.querySelector(".chooseAIscreen");
+let talkingAIscreen = document.querySelector(".talkingAIscreen");
+let confrontscreen = document.querySelector(".confrontscreen");
+let warningscreen = document.querySelector(".warningscreen");
+let futurescreen = document.querySelector(".futurescreen");
+let checkscreen = document.querySelector(".checkscreen");
+let sourcesscreen= document.querySelector(".sourcesscreen")
+
+
+
 
 
 
@@ -45,19 +64,26 @@ buttonstart.addEventListener("click", function(buttonStartFunction) {
 
 
 
+
+
+
 ok1button.addEventListener("click", function(ok1buttonFunction) {
     ok1button.style.display = "none";
   characterintro.style.display = "none";
   choosepathscreen.style.display = "block";
 
+
 });
+
 
 talkfriendbutton.addEventListener("click", function(talkfriendbuttonFunction) {
     talkfriendbutton.style.display = "none";
   choosepathscreen.style.display = "none";
   confirmpathscreen.style.display = "block";
 
+
 });
+
 
 waitbutton.addEventListener("click", function(waitbuttonFunction) {
     waitbutton.style.display = "none";
@@ -65,11 +91,13 @@ waitbutton.addEventListener("click", function(waitbuttonFunction) {
   askscreen.style.display = "block";
 });
 
+
 ok2button.addEventListener("click", function(ok2buttonFunction) {
     ok2button.style.display = "none";
   askscreen.style.display = "none";
   askmomscreen.style.display = "block";
 });
+
 
 thinkbutton.addEventListener("click", function(waitbuttonFunction) {
     thinkbutton.style.display = "none";
@@ -77,14 +105,105 @@ thinkbutton.addEventListener("click", function(waitbuttonFunction) {
   thinkscreen.style.display = "block";
 });
 
+
 ok3button.addEventListener("click", function(waitbuttonFunction) {
     ok3button.style.display = "none";
   thinkscreen.style.display = "none";
   madeupscreen.style.display = "block";
 });
 
-continuebutton.addEventListener("click", function(waitbuttonFunction) {
-    continuebutton.style.display = "none";
+
+continuebutton1.addEventListener("click", function(waitbuttonFunction) {
+    continuebutton1.style.display = "none";
   madeupscreen.style.display = "none";
   playagainscreen.style.display = "block";
 });
+
+
+talkAIbutton.addEventListener("click", function(waitbuttonFunction) {
+    talkAIbutton.style.display = "none";
+  choosepathscreen.style.display = "none";
+  chooseAIscreen.style.display = "block";
+});
+
+
+ok4button.addEventListener("click", function(waitbuttonFunction) {
+    ok4button.style.display = "none";
+  chooseAIscreen.style.display = "none";
+  talkingAIscreen.style.display = "block";
+});
+
+
+continuebutton2.addEventListener("click", function(waitbuttonFunction) {
+    continuebutton2.style.display = "none";
+  talkingAIscreen.style.display = "none";
+  confrontscreen.style.display = "block";
+});
+
+
+continuebutton3.addEventListener("click", function(waitbuttonFunction) {
+    continuebutton3.style.display = "none";
+  confrontscreen.style.display = "none";
+  warningscreen.style.display = "block";
+});
+
+
+continuebutton4.addEventListener("click", function(waitbuttonFunction) {
+    continuebutton4.style.display = "none";
+  warningscreen.style.display = "none";
+  futurescreen.style.display = "block";
+});
+
+
+continuebutton5.addEventListener("click", function(waitbuttonFunction) {
+    continuebutton5.style.display = "none";
+  futurescreen.style.display = "none";
+  playagainscreen.style.display = "block";
+});
+
+
+useAIbutton.addEventListener("click", function(waitbuttonFunction) {
+    useAIbutton.style.display = "none";
+  confirmpathscreen.style.display = "none";
+  chooseAIscreen.style.display = "block";
+});
+
+
+dbuseAIbutton.addEventListener("click", function(waitbuttonFunction) {
+    dbuseAIbutton.style.display = "none";
+  askmomscreen.style.display = "none";
+  checkscreen.style.display = "block";
+});
+
+
+continuebutton6.addEventListener("click", function(waitbuttonFunction) {
+    continuebutton6.style.display = "none";
+  checkscreen.style.display = "none";
+  madeupscreen.style.display = "block";
+});
+
+
+sourcesbutton.addEventListener("click", function(waitbuttonFunction) {
+    sourcesbutton.style.display = "none";
+  playagainscreen.style.display = "none";
+  sourcesscreen.style.display = "block";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
